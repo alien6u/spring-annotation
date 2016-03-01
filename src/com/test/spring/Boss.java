9fc8d2301f18167366fbf224ba3a1dc8b3e70bbd
alien6u <1218498720@qq.com>
2016-03-01 11:28:43 +0800

    use @Autowired(required = false) and find a question

--- src/com/test/spring/Boss.java
+++ src/com/test/spring/Boss.java
@@ -3,32 +3,39 @@ package com.test.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-
 public class Boss {
 	
 	private Car car;
 	
-	@Autowired
 	private Office office;
 	
-	public Boss(Car car, Office office) {
+	//必须要用默认构造函数才能不报错，原因见：
+	//http://forum.spring.io/forum/spring-projects/container/97753-how-to-autowire-a-bean-without-default-constructor
+	/*public Boss(Car car) {
 		super();
-		this.car = car;
-		this.office = office;
-	}
+		System.out.println("1");
+		System.out.println("2");
+	}*/
 	@Override
 	public String toString() {
 		 return "car:" + car + "\n" + "office:" + office;
 	}
 	public Car getCar() {
+		System.out.println("4");
 		return car;
 	}
+	
+	@Autowired
 	public void setCar(Car car) {
+		System.out.println("3");
 		this.car = car;
 	}
+	
 	public Office getOffice() {
 		return office;
 	}
+	
+	@Autowired(required = false)
 	public void setOffice(Office office) {
 		this.office = office;
 	}
