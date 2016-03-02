0eaca08c943b7e7f3522264d253035ce64623572
alien6u <1218498720@qq.com>
2016-03-02 09:18:46 +0800

    Autowired with Qualified to use

--- src/com/test/spring/Boss.java
+++ src/com/test/spring/Boss.java
@@ -2,11 +2,17 @@
 package com.test.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Boss {
 	
+	//构造函数已经注解了，此处可以不用注解
+	@Autowired
 	private Car car;
 	
+	//构造函数已经注解 了，此处可以不用注解
+	@Autowired
+    @Qualifier("office")
 	private Office office;
 	
 	//必须要用默认构造函数才能不报错，原因见：
@@ -16,16 +22,23 @@ public class Boss {
 		System.out.println("1");
 		System.out.println("2");
 	}*/
+	
 	@Override
 	public String toString() {
 		 return "car:" + car + "\n" + "office:" + office;
+	}
+	
+	@Autowired
+	public Boss(Car car, @Qualifier("office") Office office) {
+		super();
+		this.car = car;
+		this.office = office;
 	}
 	public Car getCar() {
 		System.out.println("4");
 		return car;
 	}
 	
-	@Autowired
 	public void setCar(Car car) {
 		System.out.println("3");
 		this.car = car;
@@ -35,8 +48,7 @@ public class Boss {
 		return office;
 	}
 	
-	@Autowired(required = false)
-	public void setOffice(Office office) {
+	public void setOffice(@Qualifier("office") Office office) {
 		this.office = office;
 	}
 	
